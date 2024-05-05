013e28a82018158a3a62e7c1e00acc45e6e3847e
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-05-05 03:57:05 +0300

    Change default terminal to Konsole

--- my_dwm/config.h
+++ my_dwm/config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-c" ,"-l" , "10", "-m", dmenumon, "-fn", dmenufont, "-nb", "#222222", "-nf", "#bbbbbb" , "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *termcmd[]  = { "konsole", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
 #include "shiftview.c"
